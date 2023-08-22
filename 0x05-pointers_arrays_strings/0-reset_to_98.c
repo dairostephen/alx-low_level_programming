d7245c72c86ec2551bffa5bd86b8b33aef437302
Dairo Stephen Olusola <dairostephen@outlook.com>
2023-08-22 14:36:13 -0700

    C program written to print putcar

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 - function that update the reference of a pointer
- *  * @n: pointer to n to be updated
- *   * Return: void
- *   */
+ * @n: pointer to n to be updated
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
