c3423efc85586ab8c4c702d98cbbcf33158f8679
Dairo Stephen Olusola <dairostephen@outlook.com>
2023-08-22 03:27:15 -0700

    Program to print Programminig is a multilingual uzzle to the user screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function start
+ *
+ * Return 0 success after printing an output
+ */
+	int main(void)
+	{
+		puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+	}
+
