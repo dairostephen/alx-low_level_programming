6df1541016fd02823ec74170e1ab2b99a83e988f
Dairo Stephen Olusola <dairostephen@outlook.com>
2023-08-26 15:29:37 -0700

    Codes completed both bash script and C programs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - function start
- *
- * Return 0 success after printing an output
- */
-	int main(void)
-	{
-		puts("Programming is like building a multilingual puzzle,\n");
-	return (0);
-	}
-
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
